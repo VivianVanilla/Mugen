3afeb8a362a92709a4f59b0d7ff869ebd907d60a
Robert Davis <robert.davis@curvedental.com>
2026-09-15 14:39:58 -0600

    random question start

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import HiraganaTable from './components/HiraganaTable'
 import ExModal from './components/examplemodal'
 import './App.css'
 
+
+const CHARS = [
+  'あ', 'い', 'う', 'え', 'お'
+]
+
+
 function App() {
   const [count, setCount] = useState(Number(localStorage.getItem('count')))
 
   useEffect(() => {
     localStorage.setItem('count', count);
   }, [count]);
+
+
+  function randIndex(arr) {
+    return Math.floor(Math.random()*arr.length);
+  }
+
+
+
 
   return (
     <>
@@ -28,6 +42,30 @@ function App() {
         </button>
       </section>
 
+      <div className="ticks"></div>
+      <div className="ticks"></div>
+
+
+
+
+
+      <div id="playArea" className="flex justify-between">
+        <div id="quiz" className="">
+          <h1>Quiz</h1>
+
+          <h2>{ CHARS[randIndex(CHARS)] }</h2>
+          <input type="text" className="bg-amber-50" />
+        </div>
+
+        <div id="upgrades">
+          <h1>Upgrades</h1>
+        </div>
+      </div>
+
+
+
+
+      <div className="ticks"></div>
       <div className="ticks"></div>
 
       <section id="next-steps">
